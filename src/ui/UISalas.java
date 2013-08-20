15f5a73a279cc489f4297ad039875490633600e5
freexploit <infotux.valerio@gmail.com>
2013-08-20 00:57:14 +0000

    beatiful changes

---
+++ src/ui/UISalas.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+import cliente.Cliente;
+import contenedor.Lista;
+import datosComunes.Accion;
+import datosComunes.ObjComunicacion;
+import datosInternos.Mensaje;
+import interfaz.Cargador;
+import interfaz.DiSala;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author christopher
+ */
+public final class UISalas implements Runnable {
+    private DiSala ventana;
+
+	public UISalas(DiSala ventana) {
+		this.ventana = ventana;
+		refrescarVentana();
+	}
+
+
+    private void refrescarVentana(){
+	//Refrescar lista usuarios
+	ObjComunicacion peticion = new ObjComunicacion();
+	peticion.setAccion(Accion.LISTAR_CONECTADOS);
+	peticion.setEntrada(ventana.getIDSala());
+	Cliente conexion = new Cliente (peticion);
+	peticion = (ObjComunicacion) conexion.getMensaje();
+	Lista tempLista = (Lista)peticion.getSalida();
+	ventana.getLstUsuarios().setModel(Cargador.cargarlistaUsuarios(tempLista));
+	//Refrescar lista mensajes
+	peticion = new ObjComunicacion(Accion.ENVIAR_MENSAJE, new Mensaje(ventana.getIDSala(), "",""));
+	conexion = new Cliente(peticion);
+	peticion = (ObjComunicacion) conexion.getMensaje();
+	Lista tempMensajes = (Lista)peticion.getSalida();
+	ventana.getTxtChat().setText(Cargador.cargarMensajes(tempMensajes));
+	
+    }
+
+    public void enviarMensaje() {
+	String mensaje = ventana.getTxtMensaje().getText().trim();
+	Mensaje paquete = new Mensaje(ventana.getIDSala(), mensaje, ventana.getLogin());
+	ObjComunicacion peticion = new ObjComunicacion();
+	peticion.setAccion(Accion.ENVIAR_MENSAJE);
+	peticion.setEntrada(paquete);
+	Cliente conexion = new Cliente(peticion);
+	refrescarVentana();
+	
+    }
+
+	public void run() {
+		refrescarVentana();
+	    try {
+		    Thread.sleep(1000);
+	    } catch (InterruptedException ex) {
+		    Logger.getLogger(UISalas.class.getName()).log(Level.SEVERE, null, ex);
+	    }
+
+	}
+	
+}
